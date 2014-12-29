a44667232c72c77d2905767fcaf1874b553e3ddb
Fred Ollinger <follinge@gmail.com>
2014-12-28 20:32:29 -0800

    Utopic unicorn support.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libktomgirl):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libktomgirl" command="/usr/bin/libktomgirl"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibktomgirl 0.0.9 libktomgirl (>> 0.0.9-0), libktomgirl (<< 0.0.9-99)
